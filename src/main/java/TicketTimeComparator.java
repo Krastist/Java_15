15486c4f6b2bbd9d3983242f1893073010e0934b
Krasnikov <krastist@yandex.ru>
2023-05-22 00:07:01 +0500

    Initial commit

---
+++ src/main/java/TicketTimeComparator.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+
+    @Override
+    public int compare(Ticket t1, Ticket t2) {
+        int flightTimeT1;
+        int flightTimeT2;
+        flightTimeT1 = t1.getTimeTo() - t1.getTimeFrom();
+        flightTimeT2 = t2.getTimeTo() - t2.getTimeFrom();
+
+        if (flightTimeT1 < flightTimeT2) {
+            return -1;
+        } else if (flightTimeT1 > flightTimeT2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/AviaSoulsTest.java
@@ -0,0 +1,9 @@
+public class AviaSoulsTest {
+    Ticket ticket1 = new Ticket("Moscov", "Surgut", 10_000, 6, 13);
+    Ticket ticket2 = new Ticket("Moscov", "Surgut", 11_000, 2, 7);
+    Ticket ticket3 = new Ticket("Moscov", "Surgut", 14_000, 4, 10);
+    Ticket ticket4 = new Ticket("Moscov", "Surgut", 13_000, 1, 5);
+    Ticket ticket5 = new Ticket("Moscov", "Surgut", 9_000, 11, 20);
+
+
+}
